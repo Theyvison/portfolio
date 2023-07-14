1aebe304825a4e9d7b9c7579117e2dbbc56d89a1
Theyvison <theyvison.jf@hotmail.com>
2023-07-14 19:29:17 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import ComingSoon from "./components/ComingSoon";
+
+function App() {
+  return (
+    <div className="h-full bg-zinc-900">
+      <ComingSoon />
+    </div>
+  );
+}
+
+export default App;
