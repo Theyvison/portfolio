7a847f2f9af6040c58bf940e56071fbe2d83dc64
Theyvison <theyvison.jf@hotmail.com>
2025-03-22 16:09:51 -0300

    feat: some simple modifications, adding animations to the page

--- main.js
+++ main.js
@@ -3,3 +3,20 @@ function handleScrollDown() {
     top: window.innerHeight,
   })
 }
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 500,
+  reset: true
+})
+
+scrollReveal.reveal(`
+  #profile, #intro-subtitle, #intro-title, #intro-text, .tags, .scroll-down, #projects-title, #projects-subtitle, .contact span, .contact h2, .contact p
+  
+  `, { interval: 100 })
+
+scrollReveal.reveal(`
+    .tag, .project, .contact ul a, footer
+    
+    `, { interval: 100 })
